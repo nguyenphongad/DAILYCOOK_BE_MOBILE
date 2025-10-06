3de9f9802ff591ce984beebaf2f9952dd0c6f2cb
HuyenTranz <tranthihuyentran982003@gmail.com>
2025-10-06 20:58:03 +0700

    feat: create redux toolkit meal #1

--- admin/src/redux/store.js
+++ admin/src/redux/store.js
@@ -1,17 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './slices/authSlice'
 import dietTypeReducer from './slices/dietTypeSlice'
-import ingredientCategoryReducer from './slices/ingredientCategorySlice'
 import ingredientReducer from './slices/ingredientSlice'
+import ingredientCategoryReducer from './slices/ingredientCategorySlice'
+import mealReducer from './slices/mealSlice'
 import mealCategoryReducer from './slices/mealCategorySlice'
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     dietType: dietTypeReducer,
-    ingredientCategory: ingredientCategoryReducer,
     ingredients: ingredientReducer,
-    mealCategory: mealCategoryReducer
+    ingredientCategory: ingredientCategoryReducer,
+    meals: mealReducer,
+    mealCategory: mealCategoryReducer,
   },
 })
 
---
+++ admin/src/redux/thunks/mealThunk.jsx
@@ -0,0 +1,168 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { get, post, put, del } from "../../services/api.service";
+import ENDPOINT from '../../constants/Endpoint';
+import { toast } from 'sonner';
+import {
+    setError,
+    setLoading,
+    setMeals,
+    setSelectedMeal,
+    addMealToList,
+    updateMealInList,
+    removeMealFromList,
+} from '../slices/mealSlice';
+
+
+
+// Lấy danh sách tất cả meal 
+export const fetchMealCategories = createAsyncThunk(
+    'meals/fetchMealCategories',
+    async (params = { page: 1, limit: 10 }, { dispatch, getState, rejectWithValue }) => {
+        try {
+            dispatch(setLoading(true));
+
+            const { token } = getState().auth;
+            const response = await get(
+                `${ENDPOINT.GET_LIST_MEAL}?page=${params.page}&limit=${params.limit}`,
+                token
+            );
+
+            if (response.status) {
+                dispatch(setMeals(response.data));
+                return response;
+            } else {
+                dispatch(setError(response.message));
+                toast.error(response.message || 'Không thể tải danh sách');
+                return rejectWithValue(response.message);
+            }
+        } catch (error) {
+            const errorMessage = error.response?.data?.message || error.message || 'Lỗi kết nối đến server';
+            dispatch(setError(errorMessage));
+            toast.error(errorMessage);
+            return rejectWithValue(errorMessage);
+        }
+    }
+);
+
+// Lấy meal  bằng id
+export const fetchMealById = createAsyncThunk(
+    'meals/fetchMeal',
+    async (id, { dispatch, getState, rejectWithValue }) => {
+        try {
+            dispatch(setLoading(true));
+
+            const { token } = getState().auth;
+            const response = await get(
+                `${ENDPOINT.GET_MEAL}/${id}}`,
+                token
+            );
+
+            if (response.status) {
+                dispatch(setSelectedMeal(response.data));
+                dispatch(setLoading(false));
+                return response.data;
+            } else {
+                dispatch(setError(response.message));
+                toast.error(response.message || 'Không thể tải danh sách');
+                return rejectWithValue(response.message);
+            }
+        } catch (error) {
+            const errorMessage = error.response?.data?.message || error.message || 'Lỗi kết nối đến server';
+            dispatch(setError(errorMessage));
+            toast.error(errorMessage);
+            return rejectWithValue(errorMessage);
+        }
+    }
+)
+
+// Thêm meal  mới với hỗ trợ upload ảnh
+export const addMeal = createAsyncThunk(
+    'meals/addMeal',
+    async (mealData, { dispatch, getState, rejectWithValue }) => {
+        try {
+            dispatch(setLoading(true));
+
+            const { token } = getState().auth;
+            const response = await post(
+                ENDPOINT.ADD_MEAL,
+                mealData,
+                token);
+
+            if (response.status) {
+                dispatch(addMealToList(response.data));
+                toast.success(response.message || 'Thêm danh mục món ăn thành công');
+                return response.data;
+            } else {
+                dispatch(setError(response.message));
+                toast.error(response.message);
+                return rejectWithValue(response.message);
+            }
+        } catch (error) {
+            const errorMessage = error.response?.data?.message || error.message || 'Lỗi kết nối đến server';
+            dispatch(setError(errorMessage));
+            toast.error(errorMessage);
+            return rejectWithValue(errorMessage);
+        }
+    }
+);
+
+// Cập meal ingredient 
+export const updateMeal = createAsyncThunk(
+    'meals/updateMeal',
+    async ({ id, mealData }, { dispatch, getState, rejectWithValue }) => {
+        try {
+            dispatch(setLoading(true));
+
+            const { token } = getState().auth;
+            const response = await put(
+                `${ENDPOINT.UPDATE_MEAL}/${id}`,
+                mealData,
+                token
+            );
+
+            if (response.status) {
+                dispatch(updateMealInList(response.data));
+                toast.success(response.message || 'Cập nhật thành công');
+                return response.data;
+            } else {
+                dispatch(setError(response.message));
+                toast.error(response.message);
+                return rejectWithValue(response.message);
+            }
+        } catch (error) {
+            const errorMessage = error.response?.data?.message || error.message || 'Lỗi kết nối đến server';
+            dispatch(setError(errorMessage));
+            toast.error(errorMessage);
+            return rejectWithValue(errorMessage);
+        }
+    }
+);
+
+// Xóa meal 
+export const deleteMeal = createAsyncThunk(
+    'meals/deleteMeal',
+    async (id, { dispatch, getState, rejectWithValue }) => {
+        try {
+            dispatch(setLoading(true));
+
+            const { token } = getState().auth;
+            const response = await del(`${ENDPOINT.DELETE_MEAL}/${id}`,
+                token
+            );
+
+            if (response.status) {
+                dispatch(removeMealFromList(id));
+                toast.success(response.message || 'Xóa thành công');
+                return id;
+            } else {
+                dispatch(setError(response.message));
+                toast.error(response.message);
+                return rejectWithValue(response.message);
+            }
+        } catch (error) {
+            dispatch(setError(error.message));
+            toast.error(error.message);
+            return rejectWithValue(error.message);
+        }
+    }
+);
